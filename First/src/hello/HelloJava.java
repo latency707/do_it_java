2651f21c53a78588b302358e181b9e1bba3bfdef
uklv9 <uklv9@DESKTOP-B1LMGOG>
2022-08-14 11:44:25 +0900

    20220814 do it J

---
+++ First/src/hello/HelloJava.java
@@ -0,0 +1,18 @@
+/*
+여
+러
+줄
+주
+석
+*/
+
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java."); // "Hello Java."  출력
+
+	}
+
+}
---
+++ First/src/hello/HelloName.java
@@ -0,0 +1,20 @@
+/*연습문제
+ * Q1) 프로그램(코드)을 기계가 이해할 수 있는 언어로 바꾸는 작업을 [컴파일] 이라고 합니다.
+ * Q2) [객체 지향 프로그래밍] 언어는 자바나 C++와 같이 대상이 되는 객체를 기반으로 프로그램을 구현하는 언어입니다.
+ * Q3) 자바로 만든 프로그램은 [자바 가상 머신; JVM]이 설치되어 있으면 운영체제와 상관없이 실행할 수 있습니다.
+ * Q4) 자바 개발을 위해 설치하는 자바 라이브러리를 [JDK; Java Development Kit]라고 합니다.
+ * Q5) 자바 프로그램이 실행되는 자바 실행 환경을 [JRE ; Java Runtime Environment]라고 합니다.
+ * Q6) 두 번째 자바 프로그램을 만들어보세요.
+ * 이클립스에서 'Hello, Java' 대신 여러분의 이름을 출력해 보세요.
+ */
+
+// Q6 이름 출력
+
+package hello;
+
+public class HelloName {
+	public static void main(String[] args) {
+		System.out.println("Hello, my name is G.I Kim.");
+	}
+
+}
